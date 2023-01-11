5bb89db0780a6483538ac2b942eccba711cc1f7a
Kairat <dzhumabaev.kai@gmail.com>
2023-01-11 15:49:44 +0600

    add error boundary

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,10 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
     state = {
         char: {},
         loading: true,
@@ -30,6 +26,10 @@ class RandomChar extends Component {
         })
     }
 
+    onCharLoading = () => {
+        this.setState({loading: true});
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -39,7 +39,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.setState({loading: true});
+        this.onCharLoading();
         this.marvelService
             .getCharacters(id)
             .then(this.onCharLoaded)
