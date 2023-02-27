06fea62728be710e42b738634bd34535347fa6b3
Kairat <dzhumabaev.kai@gmail.com>
2023-02-27 15:34:32 +0600

    fixed urls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './components/app/App';
 
 import './style/style.scss';
@@ -9,8 +8,6 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </React.StrictMode>
   );
